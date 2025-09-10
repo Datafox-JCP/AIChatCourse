32431d79ebd8b61d558d3eb73a873bfd8532c48e
Juan Carlos Pazos <datafox@outlook.com>
2025-09-10 07:22:18 -0600

    Initial Commit

---
+++ AIChatCourse/AIChatCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatCourseApp.swift
+//  AIChatCourse
+//
+//  Created by Juan Carlos Pazos on 10/09/25.
+//
+
+import SwiftUI
+
+@main
+struct AIChatCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIChatCourseTests/AIChatCourseTests.swift
@@ -0,0 +1,16 @@
+//
+//  AIChatCourseTests.swift
+//  AIChatCourseTests
+//
+//  Created by Juan Carlos Pazos on 10/09/25.
+//
+
+import Testing
+
+struct AIChatCourseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
